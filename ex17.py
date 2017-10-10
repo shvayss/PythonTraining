c80c04b09a2019a4ff85c7b779c3d148eec70c26
John Doe <john@sample.com>
2017-10-10 18:28:37 +0300

    converted from 27 to 36

--- ex17.py
+++ ex17.py
@@ -1,29 +1,29 @@
-#Exercise 17: More Files
+# Exercise 17: More Files
 
 # -*- coding: utf-8 -*-
 
-from sys import argv
 from os.path import exists
+from sys import argv
 
 script, from_file, to_file = argv
 
-print "Copynig from %s to %s" % (from_file, to_file)
+print("Copying from %s to %s" % (from_file, to_file))
 
-#we could do these two on one line too, how?
-in_file =  open(from_file)
-indata = in_file.read()
-#indata = open(from_file).read()
+# we could do these two on one line too, how?
+in_file = open(from_file)
+in_data = in_file.read()
+# in_data = open(from_file).read()
 
-print "The input file is %d bytes long" % len(indata)
+print("The input file is %d bytes long" % len(in_data))
 
-print "Does the output file exist? %r" % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort."
-raw_input()
+print("Does the output file exist? %r" % exists(to_file))
+print("Ready, hit RETURN to continue, CTRL-C to abort.")
+input()
 
 out_file = open(to_file, 'w')
-out_file.write(indata)
+out_file.write(in_data)
 
-print "Alright, all done."
-#open(to_file, 'w').write(open(from_file).read())
+print("Alright, all done.")
+# open(to_file, 'w').write(open(from_file).read())
 out_file.close()
 in_file.close()
--- ex18.py
+++ ex18.py
@@ -1,24 +1,29 @@
-#Exercise 18: Names, Variables, Code, Functions
+# Exercise 18: Names, Variables, Code, Functions
+
 
 # -*- coding: utf-8 -*-
-#this one is like your scripts with argv
+# this one is like your scripts with argv
 def print_two(*args):
-	arg1, arg2 = args
-	print "arg1: %r, arg2: %r" % (arg1, arg2)
-	
-#ok, that *args is actually pointless, we can just do this
+    arg1, arg2 = args
+    print("arg1: %r, arg2: %r" % (arg1, arg2))
+
+
+# ok, that *args is actually pointless, we can just do this
 def print_two_again(arg1, arg2):
-	print "arg1: %r, arg2: %r" % (arg1, arg2)
-	
-#this just takes one argument
+    print("arg1: %r, arg2: %r" % (arg1, arg2))
+
+
+# this just takes one argument
 def print_one(arg1):
-	print "arg1: %r" % arg1
-	
-#this one takes no arguments
+    print("arg1: %r" % arg1)
+
+
+# this one takes no arguments
 def print_none():
-	print "I got nothin'."
-	
-print_two("Zed","Shaw")
-print_two_again("Zed","Shaw")
+    print("I got nothing.")
+
+
+print_two("Zed", "Shaw")
+print_two_again("Zed", "Shaw")
 print_one("First")
 print_none()
